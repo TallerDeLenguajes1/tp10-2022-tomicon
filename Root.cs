23060c93bea871b142359113bf245e733ae6c096
emybarr <eybarreiro.2a@gmail.com>
2022-06-21 15:12:14 -0300

    cambio de nombre de una clase

---
+++ Root.cs
@@ -0,0 +1,6 @@
+// See https://aka.ms/new-console-template for more information
+public class Lista_civilizaciones
+    {
+        [JsonPropertyName("civilizations")]
+        public List<Civilization> Civilizations { get; set; }
+    }
